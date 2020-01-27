be801fb4f8bd3e67083bd490f1e310103212ba2a
Shayan Mandegarian <smandegarian@yahoo.com>
2020-01-26 18:39:07 -0800

    added nwsecheck based on neswcheck

--- hw1/ProblemA.py
+++ hw1/ProblemA.py
@@ -164,5 +164,35 @@ def neswcheck(mat,threshold):
 		vec.append(count)
 	return vec
 
+def nwsecheck(mat,threshold):
+    rows = mat.rows
+    cols = mat.cols
+    diag = []
+    x = rows - 1
+    while x >= -(rows):
+        line = []
+        for y in range(rows + cols + 1):
+            if (x + y) < rows and y < cols:
+                if (x >= 0) or ((x < 0) and y >= -x):
+                    line.append(mat[x+y][y])
+        diag.append(line)
+        x -= 1
+	#Once list of diagonals is formed, then find counts over threshold
+	vec = []
+	memo =[]
+	for i in diag:
+		count = 0
+		for j in i:
+			if j < threshold and memo:
+				memo = []
+				count += 1
+			if j >= threshold:
+				memo.append(j)
+		if memo:
+			memo = []
+			count += 1
+		vec.append(count)
+    return vec
+
 #for testing
 print(tda('sampleimg',3,4,[10,5]))
