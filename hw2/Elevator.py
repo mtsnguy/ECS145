dee4fd8819ea4ec9f65a881e5ee6f3801cb12ed4
Gavin Grey <gjgrey@ucdavis.edu>
2020-02-25 17:32:31 -0800

    Initial implementation of mean time

--- hw2/Elevator.py
+++ hw2/Elevator.py
@@ -15,32 +15,45 @@ class passClass(Process):
         # self.arrvs will be arrivals waiting for pickup
         self.arrvs = [0.0]
         self.nextArrv = None  # for debugging/code verifying
+        self.startTime = now()
     def Run(self):
         while True:
             yield hold, self, G.Rnd.expovariate(self.passArrvRate)
-            passClass.numPassengers += 1
+            self.arrvs.append(now() - self.startTime)
             if G.elevProc.asleep == True:
                 reactivate(G.elevProc)
 
 class elevatorClass(Process):
-    numTrips = 0.0
-    fullTrips = 0.0
     def __init__(self):
         Process.__init__(self)
         self.elevReturnRate = 1.0 / float(sys.argv[3])
-        self.maxPassengers = float(sys.argv[2])
+        self.maxPassengers = int(sys.argv[2])
         self.asleep = False
+        self.waitTimes = []
+        self.numTrips = 0.0
+        self.fullTrips = 0.0
     def Run(self):
         while True:
-            if passClass.numPassengers == 0:
+            self.numTrips += 1
+            #Get the first X people in the list, go to sleep if nobody there
+            passengers = G.passProc.arrvs[:self.maxPassengers]
+            if len(G.passProc.arrvs) == 0:
                 self.asleep = True
                 yield passivate,self
-            elevatorClass.numTrips += 1
-            if passClass.numPassengers > self.maxPassengers:
-                elevatorClass.fullTrips += 1
-                passClass.numPassengers -= self.maxPassengers
-            else:
-                passClass.numPassengers = 0
+                self.asleep = False
+                passengers = G.passProc.arrvs[:self.maxPassengers]
+            
+            #Remove the first X people from the list
+            G.passProc.arrvs = G.passProc.arrvs[self.maxPassengers:]
+
+            #If there are passengers left over
+            if len(G.passProc.arrvs) > 0:
+                self.fullTrips += 1
+            
+            passengers = map(lambda u: (now() - G.passProc.startTime) - u, passengers)
+            self.waitTimes.extend(passengers)
+
+            #Wait for the elevator to arrive again
             yield hold, self, G.Rnd.expovariate(self.elevReturnRate)
 
 def main():
@@ -51,6 +64,8 @@ def main():
     activate(G.passProc, G.passProc.Run())
     MaxSimTime = float(sys.argv[4])
     simulate(until=MaxSimTime)
-    print 'proportion of times elevator is full:', (elevatorClass.fullTrips / elevatorClass.numTrips)
+    print 'mean passenger wait: ', sum(G.elevProc.waitTimes) / len(G.elevProc.waitTimes)
+    print 'prop. of visits that leave passengers behind: ', (G.elevProc.fullTrips / G.elevProc.numTrips)
+
 
 if __name__ == '__main__':  main()
